c85d4715e94a47803df45c3c7f3969035c6d92df
crossxx-labs <crossxx-labs@gmail.com>
2024-02-16 20:58:39 +0800

    up

--- solathon.transfer.sol.py
+++ solathon.transfer.sol.py
@@ -3,18 +3,18 @@ from solathon import Client, Transaction, PublicKey, Keypair
 import sys
 
 """
-Usage: {sys.argv[0]} sender_private_key receiver_address
+Usage: {sys.argv[0]} sender_private_key receiver_address sol_amount
 """
 
 if __name__ == "__main__":
     if (len(sys.argv) <= 2):
-        print(f"Usage: {sys.argv[0]} sender_private_key receiver_address")
+        print(f"Usage: {sys.argv[0]} sender_private_key receiver_address sol_amount")
         sys.exit(1)
     client = Client("https://api.mainnet-beta.solana.com")
 
     sender = Keypair.from_private_key(f"{sys.argv[1]}")
     receiver = PublicKey(f"{sys.argv[2]}")
-    amount = 0.001 * 1e9
+    amount = float(sys.argv[3]) * 1e9
 
     instruction = transfer(
             from_public_key=sender.public_key,
